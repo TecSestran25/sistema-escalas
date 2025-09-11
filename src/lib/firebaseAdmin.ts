f44cf1b480b0f64ef53b69f6bdb474c501b6b5d2
TecSestran25 <tec.informacao.sestran@gmail.com>
2025-09-11 15:06:04 -0300

    melhorando leitura da key

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -2,13 +2,15 @@
 // src/lib/firebaseAdmin.ts
 import admin from 'firebase-admin';
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
       credential: admin.credential.cert({
         projectId:process.env.FIREBASE_PROJECT_ID,
         clientEmail:process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey:process.env.FIREBASE_PRIVATE_KEY
+        privateKey:privateKey
       })
     });
   } catch (error: any) {
